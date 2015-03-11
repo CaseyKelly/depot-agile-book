bbb3a728bb66b5d55d048ff0179a5755da36ddad
CaseyKelly <casey.kelly@colorado.edu>
2015-03-11 00:27:24 -0600

    add seeds for products and update products index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,23 @@ Product.create(title: 'Programming Ruby 1.9',
   image_url: 'ruby.jpg',
   price: 49.95)
 #...
+Product.create(title: 'CoffeeScript',
+  description:
+    %{<p>
+        Ruby is the fastest growing and most exciting dynamic language
+        out there. If you need to get working programs delivered fast,
+        you should add Ruby to your toolbox.
+      </p>},
+  image_url: 'cs.jpg',
+  price: 29.95)
+#...
+Product.create(title: 'Rails Test Prescriptions',
+  description:
+    %{<p>
+        Ruby is the fastest growing and most exciting dynamic language
+        out there. If you need to get working programs delivered fast,
+        you should add Ruby to your toolbox.
+      </p>},
+  image_url: 'rtp.jpg',
+  price: 19.95)
+#...
